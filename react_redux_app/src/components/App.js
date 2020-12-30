23e5d9c73829fc425376e3d5da9ed2ec9e8068bb
obata-n <akdh56akgh@gmail.com>
2020-12-30 15:39:20 +0900

    implement component with connect function

--- react_redux_app/src/components/App.js
+++ react_redux_app/src/components/App.js
@@ -1,31 +1,40 @@
 import React, { Component } from 'react'
-import { render } from 'react-dom'
+import { connect } from 'react-redux'
 
-const App = () => (<Counter></Counter>)
+import { increment, decrement } from '../actions'
 
-clas Counter extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { count: 0 }
-  }
-
-  handlePlusButton = () => {
-    this.setState({ count: this.state.count + 1 })
-  }
-
-  handleMinusButton = () => {
-    this.setState({ count: this.state.count - 1 })
-  }
+class App extends Component {
+  // 初期化はreducerで行う
+  // constructor(props) {
+  //   super(props)
+  //   this.state = { count: 0 }
+  // }
+  // handlePlusButton = () => {
+  //   this.setState({ count: this.state.count + 1 })
+  // }
+  // handleMinusButton = () => {
+  //   this.setState({ count: this.state.count - 1 })
+  // }
 
   render() {
+    const props = this.props
+
     return (
       <React.Fragment>
-        <div>count: { this.state.count }</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>value: { props.value }</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     )
   }
 }
 
-export default App;
+const mapStateToProps = state => ({ value: state.count.value })
+
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement())
+// })
+const mapDispatchToProps = ({ increment, decrement })
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- react_redux_app/src/index.js
+++ react_redux_app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from "react-redux";
 import './index.css';
+import reducer from './reducers'
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
